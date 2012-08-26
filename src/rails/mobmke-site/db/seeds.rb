9f2409ea7404f20f5df0fb27919effcd261170a0
cjibo <cgibeaut@insomdev.com>
2012-08-26 14:59:35 -0500

    Heroku and Android

--- src/rails/mobmke-site/db/seeds.rb
+++ src/rails/mobmke-site/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@role = Role.create(:name => "admin", :description => "Administration Role")
+@user = User.create(:email => "cgibeaut@insomdev.com", :password => "TestMobileMKE")
+@user.roles << @role
+
+10.times do |i|
+	Meeting.create(:title => "Meeting " + i.to_s, :description => "Generated Meeting " + i.to_s, 
+		:location => "Location " + i.to_s, :speaker => "Speaker " + i.to_s, 
+		:start_time => DateTime.now.advance(:hours => i), 
+		:end_time => DateTime.now.advance(:hours => i + 1))
+end
