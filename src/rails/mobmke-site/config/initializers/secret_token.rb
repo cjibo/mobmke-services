6848a2d5520e23d88cb6c85bc59a42c6f0373fde
cjibo <cgibeaut@insomdev.com>
2012-08-26 09:56:35 -0500

    Added bare ruby project

---
+++ src/rails/mobmke-site/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobmkeSite::Application.config.secret_token = 'a7c0547e2937191a44edb56ed04cc73f2d7b9c7d3cfdd31017cdabb061c81a35c067b88640197962f470dc9ef2a7977c2a919ebfacc028aaef6267ca8eef8694'
